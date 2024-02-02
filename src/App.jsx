b2986aa112ea837ae4ef20aaa0d96851bcc9364a
AndrewJamesC <130733134+AndrewJamesC@users.noreply.github.com>
2024-02-02 19:50:22 +1300

    Create homepage hero image

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 import "./App.css";
 
 function App() {
   return (
-    <>
+    <div className="app-container">
       <Navbar />
-    </>
+      <Home />
+    </div>
   );
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,33 @@
+import styles from "../styles/Home.module.css";
+import headphonesHero from "../assets/images/headphones-hero.png";
+
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className={styles.heroImageContainer}>
+        <img
+          className={styles.heroImg}
+          src={headphonesHero}
+          alt="Headphones hero image"
+        />
+        <div className={styles.heroTitleTextContainer}>
+          <p className={styles.brandname}>Sonic Aura</p>
+          <p className={styles.wireless}>Wireless</p>
+          <p className={styles.headphones}>HEADPHONES</p>
+        </div>
+        <div className={styles.descriptionContainer}>
+          <p className={styles.description}>Description</p>
+          <p className={styles.descriptionText}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor.
+          </p>
+        </div>
+        <button type="button" className={styles.categoryButton}>
+          Shop By Category
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
